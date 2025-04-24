56562fced13fd9ac055dba4ea6165b10eaffe204
Hariharan Selvaraj <hariharan.2309.s@icloud.com>
2025-04-24 11:21:22 +0200

    fixed the storybook error

--- packages/docs/.storybook/main.ts
+++ packages/docs/.storybook/main.ts
@@ -6,23 +6,23 @@ import { join, dirname } from 'path';
  * This function is used to resolve the absolute path of a package.
  * It is needed in projects that use Yarn PnP or are set up within a monorepo.
  */
-function getAbsolutePath(value: string): string {
+const getAbsolutePath = (value: string): string => {
   return dirname(require.resolve(join(value, 'package.json')));
-}
+};
 
 const config: StorybookConfig = {
   stories: ['../src/**/*.mdx', '../src/**/*.stories.@(js|jsx|mjs|ts|tsx)'],
   addons: [
-    getAbsolutePath('@storybook/addon-essentials'),
-    getAbsolutePath('@storybook/addon-onboarding'),
-    getAbsolutePath('@chromatic-com/storybook'),
-    getAbsolutePath('@storybook/experimental-addon-test'),
     '@storybook/addon-essentials',
+    '@storybook/addon-onboarding',
+    '@chromatic-com/storybook',
+    '@storybook/experimental-addon-test',
     '@storybook/addon-a11y',
   ],
   framework: {
-    name: getAbsolutePath('@storybook/react-vite'),
+    name: '@storybook/react-vite',
     options: {},
   },
 };
+
 export default config;
